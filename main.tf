0aaca3311e54a59ecc17119e301c16d7aa8b628e
magesh-vvs <145098559+magesh-vvs@users.noreply.github.com>
2025-10-21 00:47:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "mgh-ec2" {
-  ami           = "ami-06fa3f12191aa3337" # us-west-2
+  ami           = "ami-06fa3f12191aa3337" # ap-south-1
   instance_type = "t3.micro"
   tags = {
       Name = "TF-Instance"
